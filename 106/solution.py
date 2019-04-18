c632fef01d7d02732ba9e9c34489e8075aac3c04
Gyunghee <gyunghee0park@gmail.com>
2019-04-19 00:54:51 +0900

    Added 106

---
+++ 106/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not inorder:
+            return None
+        root = TreeNode(postorder[-1])
+        i = inorder.index(postorder[-1])
+        root.left = self.buildTree(inorder[:i], postorder[:i])
+        root.right = self.buildTree(inorder[i+1:], postorder[i:-1])
+        return root
+        
