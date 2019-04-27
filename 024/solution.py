4d1bdd7c6c91f9fc485f0629fc1b6c5bfdca92b4
Gyunghee <gyunghee0park@gmail.com>
2019-04-27 20:15:08 +0900

    Added 024

---
+++ 024/solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        if not head.next:
+            return head
+        rest = head.next.next
+        tmp = head.next
+        head.next = self.swapPairs(rest)
+        tmp.next = head
+        return tmp
+        
