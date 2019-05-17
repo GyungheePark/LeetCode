3a8f9eaf0179088a5fa0c2e027fd975c42495ab6
Gyunghee Park <gyunghee0park@gmail.com>
2019-05-17 14:59:42 +0900

    Added 206 iterative solution

---
+++ 206/itersolution.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        cur = head
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+        return prev
+        
