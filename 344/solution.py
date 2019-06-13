48324395499da3c87d024ec58a27bfca88a2f766
Gyunghee Park <gyunghee0park@gmail.com>
2019-06-13 11:09:22 +0900

    Added 344

---
+++ 344/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)/2):
+            s[i], s[-i-1] = s[-i-1], s[i]
